f5d6d310006cf25fdb3dd97317dd028fbe623a0b
Aline Victória <37562315+alinevictoria@users.noreply.github.com>
2018-05-09 22:56:19 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int ab, char ** ac) 
+{
+	
+	
+
+    char numero[256];
+	
+    int x=0,a;
+	
+    printf("Digite a string\n");
+	
+    scanf("%s",&numero);
+	
+    while(numero[x]!='\0')
+{
+		
+         a=48;
+		
+         while(a>=48 && a<=57){
+			
+              if(numero[x]==a) printf("%c",numero[x]);
+			
+              a++;
+		
+         }
+		
+         x++;
+	
+     }
+
+}
