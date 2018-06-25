b15aa898b46ce467a48c85996ff69bee28abae13
Aline Victória <37562315+alinevictoria@users.noreply.github.com>
2018-06-24 21:57:39 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,59 +1,75 @@
-//Aluna: Aline Victória Machado Silva - 11721EBI006
+//Aluna: Aline VictÃ³ria Machado Silva - 11721EBI006
 
 #include <stdio.h>
 
-int main(){
+int main () 
+{
+ int i = 0, estado = 0;
+ char bits[256];	
 	
-	int estado = 0, i = 0;
-    char bits[256];
-    
-    printf("Digite um numero binario: \n");
-    scanf("%s", bits);
-    
-    while(bits[i] != '\0'){
-	
-        if (estado == 0 && bits[i] == '0'){
-    		estado = 0;
-    		i++;
-		}
-    	
-    	if (estado == 0 && bits[i] == '1'){
-    		estado = 1;
-    		i++;
-    	}
-    	
-    	if (estado == 1 && bits[i] == '0'){
-    		estado = 2;
-    		i++;
-    	}
-    	
-    	if (estado == 1 && bits[i] == '1'){
-    		estado = 0;
-    		i++;
-    	}
-    	
-    	if (estado == 2 && bits[i] == '0'){
-    		estado = 1;
-    		i++;
-    	}
-    	
-    	if (estado == 2 && bits[i] == '1'){
-    		estado = 2;
-    		i++;
-    	}
-	
+  printf ("Programa para verificar se um numero eh multiplo de 3");
+  printf ("\n Digite um numero binario: ");
+
+  scanf ("%s", &bits[i]);	
+
+  while (bits[i] != '\0'){
+
+      if (bits[i] != '0' && bits[i] != '1')	
+
+{
+     printf ("Erro");
+     
+	 return 1;		 
+}
+
+if (estado == 0 && bits[i] == '0')
+
+	{
+	 estado = 0;		
 	}
-	
-	if (estado == 0){
-		printf("\nO numero binario eh: %s", &bits);
-		printf("\nO numero e multiplo de 3.");
+
+      else if (estado == 0 && bits[i] == '1')
+	{
+	  estado = 1;	
 	}
-	
-	else{
-		printf("\nO numero binario e: %s", &bits);
-		printf("\nO numero nao e multiplo de 3.");
+
+       else if (estado == 1 && bits[i] == '0')
+	{
+	  estado = 2;	
 	}
-	
-	return 0;
-	
+	  
+      else if (estado == 2 && bits[i] == '1')	
+	{
+	  estado = estado;	
+	}
+
+      
+      else if (estado == 2 && bits[i] == '0')	
+	{
+	  estado = 1;	
+	}
+
+      else if (estado == 1 && bits[i] == '1')	
+	{
+	  estado = 0;	
+	}
+else			
+	{
+	estado = estado;
+}
+
+i++;			
+}
+	  if (estado == 0)
+ {
+     printf("O numero eh multiplo de 3\n");
+ }
+
+ else 
+
+ {
+     printf("O numero nao eh multiplo de 3");
+ }
+
+ return 0;
 }
