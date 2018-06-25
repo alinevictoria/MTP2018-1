27fb76247a1bf331bc6a8c9d3124c1688e6095b6
Aline Victória <37562315+alinevictoria@users.noreply.github.com>
2018-06-24 22:16:42 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -1,34 +1,35 @@
 //Aluna: Aline Victória Machado Silva - 11721EBI006
+
 #include <stdio.h>
 
-int main(int ab, char ** ac) 
-{
-	
-	
+int main()
 
-    char numero[256];
-	
-    int x=0,a;
-	
-    printf("Digite a string\n");
-	
-    scanf("%s",&numero);
-	
-    while(numero[x]!='\0')
 {
-		
-         a=48;
-		
-         while(a>=48 && a<=57){
-			
-              if(numero[x]==a) printf("%c",numero[x]);
-			
-              a++;
-		
-         }
-		
-         x++;
-	
-     }
+
+	int sl=0, i=0;
+
+	char numero[256];
+
+	printf("Digite uma sequência de caracteres numericos: ");
+
+	scanf("%s", numero);
+
+	getchar();
+
+	for (i=0; numero[i]; i++)
+
+	{
+
+		if ((numero[i]-'0')>=0 && (numero[i]-'0')<=9)
+
+			sl=sl*10+(numero[i]-'0');
+
+		else;
+
+	}
+
+	printf("%i", sl);
+
+	return 0;
 
 }
